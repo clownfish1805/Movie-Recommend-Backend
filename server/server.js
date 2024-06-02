fd8158563383a4e071367be51e9504d33146e663
akkshara <akkshara.ss2021cce@sece.ac.in>
2024-06-02 23:42:24 +0530

    deppp 1

--- server/server.js
+++ server/server.js
@@ -5,14 +5,9 @@ const axios = require('axios');
 const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 
-
-
 const app = express();
 const PORT = 5000;
 
-const cors = require('cors');
-
-// Allow multiple origins by setting a function for the origin property
 const allowedOrigins = ['https://deploy-mern-1whq.vercel.app', 'https://movie-recommend-frontend.vercel.app'];
 
 app.use(cors({
@@ -28,29 +23,26 @@ app.use(cors({
     credentials: true
 }));
 
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.send("Server");
-})
-
+});
 
 const decodeToken = (token) => {
     try {
-        //jwt.decode(token);
         return jwt.decode(token);
-  } catch (error) {
-    console.error("Invalid token", error);
-    return null;
-  }
+    } catch (error) {
+        console.error("Invalid token", error);
+        return null;
+    }
 };
-
 
 const API_KEY = 'cc28fc43';
 const BASE_URL = 'http://www.omdbapi.com/';
 
 const MONGO_URI = 'mongodb+srv://akksharass:dbUserPassword@cluster0.z3qrcab.mongodb.net/mydatabase?retryWrites=true';
 console.log('Mongo URI: ', MONGO_URI);
-
 
 if (!MONGO_URI) {
     console.error('MONGO_URI is not defined. Please check your .env file.');
@@ -64,14 +56,6 @@ mongoose.connect(MONGO_URI, {
   console.log('MongoDB connected...'))
   .catch(err => console.error('MongoDB connection error:', err));
 
-
-
-// Enable CORS and parse json body
-app.use(cors());
-app.use(express.json());
-
-
-//schema
 const userSchema = new mongoose.Schema({
     username: String,
     email: String,
@@ -83,13 +67,9 @@ const favSchema = new mongoose.Schema({
     movie: String
 });
 
-
-// model
 const User = mongoose.model('users', userSchema);
 const Fav = mongoose.model('favs', favSchema);
 
-
-// Middleware to authenticate token
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
@@ -104,50 +84,40 @@ const authenticateToken = (req, res, next) => {
 
 // Sign-in route
 app.post('/api/signin', async (req, res) => {
-    console.log("entered siginin");
+    console.log("entered signin");
     const { email, password } = req.body;
     console.log("email", email);
 
     try {
-         // Check if user exists with provided email
         const user = await User.findOne({ email });
         console.log(user);
         if (!user) {
             return res.status(401).json({ error: 'Invalid email or password' });
         }
 
-        console.log("Checked password");
-        // Verify password
         if (user.password !== password) {
             return res.status(401).json({ error: 'Invalid email or password' });
         }
 
-        // If user exists and password matches, generate JWT token
         const token = jwt.sign({ email: user.email, id: user._id }, 'secret_key');
         console.log(token);
         res.json({ token });
-        
     } catch (error) {
         console.error('Error signing in:', error);
         res.status(500).json({ error: 'Internal server error' });
     }
 });
 
-
-
-
-  // Sign-up route
+// Sign-up route
 app.post('/api/signup', async (req, res) => {
     const { username, email, password } = req.body;
 
     try {
-        // Check if user already exists
         const existingUser = await User.findOne({ email });
         if (existingUser) {
             return res.status(400).json({ error: 'User already exists' });
         }
 
-        // Create a new user document
         const newUser = new User({ username, email, password });
         await newUser.save();
 
@@ -158,38 +128,33 @@ app.post('/api/signup', async (req, res) => {
     }
 });
 
-
-
-//Add to favorite
-app.post('/api/favorite', async (req, res) => {
+// Add to favorite
+app.post('/api/favorite', authenticateToken, async (req, res) => {
     console.log(req.body);
     const token = req.headers.authorization;
     try {
         const varToken = decodeToken(token);
 
         const email = varToken['email']; 
-
         const movie = JSON.stringify(req.body);
 
         console.log(movie);
 
         const newFav = new Fav({ email, movie });
 
-        newFav.save();
+        await newFav.save();
 
         console.log("Data saved");
 
         res.status(200).json(movie);
-
     } catch (error) {
         console.log(error);
         res.status(500).json({ error: error.message });
     }
 });
 
-
-app.get('/api/favorite', async (req, res) => {
-    console.log(req.body);
+// Get favorite
+app.get('/api/favorite', authenticateToken, async (req, res) => {
     const token = req.headers.authorization;
     try {
         const varToken = decodeToken(token);
@@ -207,48 +172,44 @@ app.get('/api/favorite', async (req, res) => {
     }
 });
 
-
 // Movie Search Route
-app.get('/api/search', async (req, res) => {
-    const { name,title } = req.query;
+app.get('/api/search', authenticateToken, async (req, res) => {
+    const { name, title } = req.query;
     const token = req.headers.authorization;
     try {
         decodeToken(token);
         console.log(decodeToken(token));
         if (title == null) {
             const response = await axios.get(BASE_URL, {
-            params: {
-                apikey: API_KEY,
-                s: name
-            }
+                params: {
+                    apikey: API_KEY,
+                    s: name
+                }
             });
             console.log(response.data);
-        if (response.data.Response === 'True') {
-            res.json(response.data);
-        } else {
-            throw new Error(response.data.Error);
-        }
-        }
-        else {
-            const completeResponse = await axios.get(BASE_URL, {
-            params: {
-                apikey: API_KEY,
-                t: name
+            if (response.data.Response === 'True') {
+                res.json(response.data);
+            } else {
+                throw new Error(response.data.Error);
             }
+        } else {
+            const completeResponse = await axios.get(BASE_URL, {
+                params: {
+                    apikey: API_KEY,
+                    t: name
+                }
             });
             console.log(completeResponse.data);
-        if (completeResponse.data.Response === 'True') {
-            res.json(completeResponse.data);
-        } else {
-            throw new Error(completeResponse.data.Error);
+            if (completeResponse.data.Response === 'True') {
+                res.json(completeResponse.data);
+            } else {
+                throw new Error(completeResponse.data.Error);
+            }
         }
-        }
-        
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
